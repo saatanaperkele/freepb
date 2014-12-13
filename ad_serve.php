2c390e02c673a0d04f073e1228a7717483df57ee
saatanaperkele <finlandia@8chan.co>
2014-12-13 15:53:13 -0600

    initial support for self-provided banner ads

---
+++ ad_serve.php
@@ -0,0 +1,15 @@
+<?php
+/*
+*	freePB
+*	A Simple, Modular, and Quick
+*	clone of ThePirateBay
+*	written on this day of great
+*	jimmy-rustling as even brokep
+*	knows what a sad state of afairs
+*	plagues our once great movement
+*	
+*
+*	File: ad_serve.php [serves banner ads]
+*	Author: finlandia
+*	Released under WTFPL
+*/
