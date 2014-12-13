d2586ce5ee4ae93546a00cf02bb69f544f0816b0
saatanaperkele <finlandia@8chan.co>
2014-12-13 12:47:20 -0600

    Making more work for myself, but anonymous uploads supported

---
+++ anonupload.php
@@ -0,0 +1,15 @@
+<?php
+/*
+*	freePB
+*	A Simple, Modular, and Quick
+*	clone of ThePirateBay
+*	written on this day of great
+*	jimmy-rustling as even brokep
+*	knows what a sad state of afairs
+*	plagues our once great movement
+*	
+*
+*	File: anonupload.php [Anonymously upload a torrent]
+*	Author: finlandia
+*	Released under WTFPL
+*/
