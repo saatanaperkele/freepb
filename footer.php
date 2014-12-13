28b771c2c8b0f7ab47956226551313d40cf81fbe
saatanaperkele <finlandia@8chan.co>
2014-12-13 15:03:43 -0600

    Linkified some items in the footer; credit where credit is due

--- footer.php
+++ footer.php
@@ -15,4 +15,4 @@
 *	Released under WTFPL
 */
 
-echo '<small><center> [freepb, <img src="/img/c_mini_7.gif" href="http://kopimi.com/kopimi"> + wtfpl, 2014, finlandia]</center></small>';
+echo '<small><center> [<a href="http://github.com/saatanaperkele/freepb">freepb</a>, <a href="http://kopimi.com/kopimi"><img src="/img/c_mini_7.gif" border="0"></a> + <a href="http://wtfpl.com">wtfpl</a>, 2014, <a href="mailto:finlandia@8chan.co">finlandia</a>]</center></small>';
