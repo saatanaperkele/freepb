6fe203859ca05157f5b260793a649620e75c7e3c
saatanaperkele <finlandia@8chan.co>
2014-12-13 14:25:49 -0600

    Added recently indexed page to show what we have and where it's from

---
+++ recently_indexed.php
@@ -0,0 +1,14 @@
+/*
+*	freePB
+*	A Simple, Modular, and Quick
+*	clone of ThePirateBay
+*	written on this day of great
+*	jimmy-rustling as even brokep
+*	knows what a sad state of afairs
+*	plagues our once great movement
+*	
+*
+*	File: recently_indexed.php [shows torrents recently indexed and from where]
+*	Author: finlandia
+*	Released under WTFPL
+*/
