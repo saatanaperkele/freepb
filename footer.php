47e58ad54fe760f17e2a5700d1e28322cc330c9a
saatanaperkele <finlandia@8chan.co>
2014-12-13 18:23:02 -0600

    Moved echos to a function, needs to be called from all footer-inclusive pages

--- footer.php
+++ footer.php
@@ -14,5 +14,7 @@
 *	Author: finlandia
 *	Released under WTFPL
 */
+function createFooter(){
 echo "<footer>";
 echo '<small><center> [<a href="http://github.com/saatanaperkele/freepb">freepb</a>, <a href="http://kopimi.com/kopimi"><img src="/img/c_mini_7.gif" border="0"></a> + <a href="http://wtfpl.net">wtfpl</a>, 2014, <a href="mailto:finlandia@8chan.co">finlandia</a>]</center></small>';
+}
