c2028ba31c3cae83fc5da4595047aa992e80a84b
saatanaperkele <finlandia@8chan.co>
2014-12-13 20:03:40 -0600

    Fixed searchbox

--- header.php
+++ header.php
@@ -27,9 +27,8 @@ global $logo;
 <img src="/img/examplebanner.gif">
 </div>
 <!-- script type="text/javascript" src="http://srvpub.com/adServe/banners?tid=SP1PIRATE_23860_0&size=468x60"></script>	</div -->
+</div>
 <form id="piracy" action="search_sanitize.php">
-<br/><input type="search" title="Pirate Search" name="q" required placeholder="Search here..." value="" style="background-color:#ffffe0;" class="searchBox"/><input 
-value="Pirate Search" type="submit" class="submitbutton"/> 
 <a href="/" class="img"><img src="<?php echo getCurrent("x", "y"); ?>" height=82 width=87 id="TPBlogo" alt="The Pirate Bay"/></a>
 <b><a href="/" title="Search Torrents">Search Torrents</a></b>&nbsp;&nbsp;|&nbsp;
 <a href="/browse" title="Browse Torrents">Browse Torrents</a>&nbsp;&nbsp;|&nbsp;
@@ -37,3 +36,6 @@ value="Pirate Search" type="submit" class="submitbutton"/>
 <a href="/tv/" title="TV shows">TV shows</a>&nbsp;&nbsp;|&nbsp;
 <a href="/music" title="Music">Music</a>&nbsp;&nbsp;|&nbsp;
 <a href="/top" title="Top 100">Top 100</a>
+<br><br/><input type="search" title="Pirate Search" name="q" required placeholder="Search here..." value="" style="background-color:#ffffe0;" class="searchBox"/><input
+value="Pirate Search" type="submit" class="submitbutton"/>
+
