1f9819fb5958a3313a35710e71fb8512d1d96b5a
saatanaperkele <finlandia@8chan.co>
2014-12-13 20:53:32 -0600

    Formatted entries for recent torrents in the table

--- recent.php
+++ recent.php
@@ -14,9 +14,11 @@
 *	Released under WTFPL
 */
 include("header.php");
+setTitle("Recent Torrents");
 headerize("Recent Torrents");
 //<!-- Building header -->
 
+//Building the top menu
 echo '<div id="content">';
 echo '<div id="sky-right">';
 echo '</div>';
@@ -30,9 +32,26 @@ echo '<div class="viewswitch"> RSS: <a href="/recentfeed.rss">feed here.</a></di
 echo '<!-- div class=""> </div></th -->';
 echo '<th><abbr title="seeders">SE</abbr></th>';
 echo '<th><abbr title="Leechers">LE</abbr></th>';
-echo '</table>';
+echo '</thread>';
 
+//block
+$category = "TV";
+$torrentid = "134";
+$torrentTitle = "TPB AFK: The Story of The Pirate Bay";
+$magnetlink = "asDF34tgfqaG46626";
+$uploader = "John33";
+$timeformatted = "Today, at 15:51";
+$size = "150";
+$seeders = 12;
+$leechers = 156;
+$time = $size;
+
+//The loop will repeat these values
+echo '<tr><td class="vertTh"><center><a href="#" title="More from this category">'.$category.'</a><br/></center></td>';
+echo '<td><div class="detName"><a href="/torrent/'.$torrentid.'"class="detLink" title="'.$torrentTitle.'">'.$torrentTitle.'</a></div>';
+echo '<a href="magnet:?'.$magnetlink.'" title="Download this!"><img src="/img/icon-magnet.gif"><img src="/static/img/dl.gif" class="dl" alt="Download"/></a><a href="/user/'.$uploader.'"><img src="/static/img/11x11p.png"/><font class="detDesc">Uploaded '.$timeformatted.'  '.$size.'<span>MB</span>, ULed by <a class="detDesc" href="/user/'.$uploader.'/" title="Browse '.$uploader.'">'.$uploader.'</a></font></td><td align="right">'.$seeders.'</td><td align="right">'.$leechers.'</td></tr>';
+echo '</table>';
 include("footer.php");
-echo "h";
+
 createFooter();
 ?>
