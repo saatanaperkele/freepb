d6ac3604288d53502a6c84575c0fd58ff3d9975b
Jukka of Finlandia <finlandia@8chan.co>
2015-01-04 00:48:47 +0000

    Created scraper.php, will run on cron and scrape for new torrents

---
+++ scraper.php
@@ -0,0 +1,15 @@
+<?php
+/*
+*	freePB
+*	A Simple, Modular, and Quick
+*	clone of ThePirateBay
+*	written on this day of great
+*	jimmy-rustling as even brokep
+*	knows what a sad state of afairs
+*	plagues our once great movement
+*	
+*
+*	File: scraper.php [ran on a cron job to get new torrents]
+*	Author: finlandia
+*	Released under WTFPL
+*/
