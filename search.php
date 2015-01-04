c782a68912e5e2c153056149a94a64588d723c29
Jukka of Finlandia <finlandia@8chan.co>
2015-01-04 23:08:44 +0000

    prepping to fix #1, also working on potential enhancement

--- search.php
+++ search.php
@@ -18,8 +18,31 @@ $term = $_GET['piracy'];
 setTitle("Search Results for $term");
 headerize("Search Results for $term");
 
+/*
+TODO: Block for handling second++ page of results
+*/
 
+//Find out how many results to display, this should be 
+//functionized in the future and applied to all table pages
+//and $defNumResults set in a central locations
+$defNumResults = "100";
+if(isset($_COOKIE['number_results'])){
+    $numResults = $_COOKIE['number_results'];
+}
+if(!isset($_COOKIE['number_results'])){
+    $numResults = $defNumResults;
+}
+//Now let's build the query
+$sql_query = "SELECT * FROM ... LIKE ... LIMIT $numResults";
 
+//table head here
 
+/*while($db->list()){
+    //table entry
+}
+*/
+
+//table end
+// Next links
 include('footer.php');
 createFooter();
