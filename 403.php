30c1063b83923be6f572bcf54bd0be5f63b3dd27
Jukka of Finlandia <finlandia@8chan.co>
2015-01-04 22:39:35 +0000

    Working on #13, halfway done

---
+++ 403.php
@@ -0,0 +1,25 @@
+<?php
+/*
+*	freePB
+*	A Simple, Modular, and Quick
+*	clone of ThePirateBay
+*	written on this day of great
+*	jimmy-rustling as even brokep
+*	knows what a sad state of afairs
+*	plagues our once great movement
+*	
+*
+*	File: 
+*	Author:
+*	Released under WTFPL
+*/
+echo("<html><head>");
+include("header.php");
+setTitle("404");
+headerize("File not found.");
+?>
+<center><img src="/img/logo.png"></center>
+<?php
+include("footer.php");
+
+createFooter();
---
+++ 404.php
@@ -0,0 +1,36 @@
+<?php
+/*
+*	freePB
+*	A Simple, Modular, and Quick
+*	clone of ThePirateBay
+*	written on this day of great
+*	jimmy-rustling as even brokep
+*	knows what a sad state of afairs
+*	plagues our once great movement
+*	
+*
+*	File: 
+*	Author:
+*	Released under WTFPL
+*/
+echo("<html><head>");
+include("functions.php");
+setTitle("Header");
+
+/*
+P
+A
+G
+E
+
+C
+O
+N
+T
+E
+N
+T
+*/
+include("footer.php");
+
+createFooter();
