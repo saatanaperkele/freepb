131f3b9cb7dc37711c08f7f046c639475f5e3ca9
jukka_keksec <dasid@goat.si>
2015-01-11 10:26:35 -0600

    Added basic content to report system

--- report.php
+++ report.php
@@ -8,7 +8,23 @@
 *	plagues our once great movement
 *	
 *
-*	File: 
-*	Author:
+*	File: report.php [Report a torrent for something illegal lol]
+*	Author: finlandia
 *	Released under WTFPL
 */
+include("header.php");
+$id = $_GET['id'];
+setTitle("Reporting torrent: #$id");
+
+if(!$_POST['send']){
+	$torrentName = getTitlefromTorrentID($id);
+	headerize("Reporting torrent: $torrentName (#$id)");
+	//draw up the page
+}
+if($_POST['send']){
+	headerize("Torrent reported!");
+	echo "Thank you for your report. It has been submitted to the $siteName staff<br>";
+	echo "If the torrent is verified to be illegal, it will be dealt with";
+	echo "accordingly. ";
+}
+
